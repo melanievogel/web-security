07a2428e49c096e3740aa058de24d628734dfc80
melanie <vogel.melanie@outlook.com>
2021-10-24 23:33:42 +0200

    feat: Both docker images running now and communicate.

--- exampleimage/express-server/server.js
+++ exampleimage/express-server/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var sanitizer = require('sanitizer');
 var cors = require('cors');
 var app = express();
+var port = 3000;
 
 var dbObj = require('./database.js');
 app.use(cors());
@@ -394,7 +395,6 @@ app.use(function (req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000.');
+app.listen(port, '0.0.0.0', function () {
+    console.log('Example app listening on port ' + port);
 });
-
